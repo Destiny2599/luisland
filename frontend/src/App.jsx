1e48159b91d7cd197c0b1768f5caee23edc8bfae
Luis <luisvoz2599@gmail.com>
2026-03-19 15:08:51 -0700

    feat: Titulos diferentes en las dos ramas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="min-vh-100 d-flex align-items-center justify-content-center">
       <h1 className="display-1 fw-bold text-primary">
-        ¡Bienvenido macbook!
+        ¡Bienvenido master!
       </h1>
     </div>
   )
